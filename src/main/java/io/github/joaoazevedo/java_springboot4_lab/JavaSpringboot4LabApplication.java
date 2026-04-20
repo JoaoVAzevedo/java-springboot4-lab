60a128d81702e8ddadeed5b7273e21a76c313d99
Azevedo <Joao.Azevedo@br.experian.com>
2026-04-20 17:26:33 -0300

    Project Created

---
+++ src/main/java/io/github/joaoazevedo/java_springboot4_lab/JavaSpringboot4LabApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaoazevedo.java_springboot4_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringboot4LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringboot4LabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/joaoazevedo/java_springboot4_lab/JavaSpringboot4LabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaoazevedo.java_springboot4_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringboot4LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
